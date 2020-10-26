62b23e6091b8f19c21c542c8c94068d68f2330c3
Michael Read <mrea0002@student.monash.edu>
2020-10-26 11:46:01 +1100

    finished version

--- server.js
+++ server.js
@@ -1,43 +1,48 @@
 const express = require('express');
 const path = require('path');
+
 let app = express();
 let server = require('http').Server(app);
-
 let io = require('socket.io')(server);
-
 let port = 8080;
 
 app.use("/", express.static(path.join(__dirname, "dist/votingsystem")));
 
+//dynamic object -- can change variables/objects without breaking the system
 let pollObj = {
     question: "Select Your Favourite Component",
     options: [
-        { text: "Angular", value: 0, count: 1 },
-        { text: "MongoDB", value: 1, count: 0 },
-        { text: "Express.js", value: 2, count: 0 },
-        { text: "Golang", value: 3, count: 0 },
-        { text: "Python", value: 4, count: 0 },
-        { text: "C#", value: 5, count: 0 },
-        { text: "PhP", value: 6, count: 0 },
-        { text: "C++", value: 7, count: 0 },
+        { text: "Angular", count: 0 },
+        { text: "MongoDB", count: 0 },
+        { text: "Express.js", count: 0 },
+        { text: "Golang", count: 0 },
+        { text: "Python", count: 0 },
+        { text: "C#", count: 0 },
+        { text: "PhP", count: 0 },
+        { text: "C++", count: 0 },
     ],
 };
 
+//when a device is connected run this
 io.on('connection', socket => {
     console.log('new connection made from client with ID = ' + socket.id);
+    //send this event to all connections
     io.sockets.emit('pollObjectEvent', { pollObject: pollObj, labels: getLabels(), values: getValues() } );
 
+    //when event newVoteEvent is triggered run this
     socket.on('newVoteEvent', data => {
         incrementPoll(data);
-        console.log('the user voted for: ', data)
+        console.log('user ' + socket.id + ' voted for: ', data)
         io.sockets.emit('pollObjectEvent', { pollObject: pollObj, labels: getLabels(), values: getValues() } );
     })
 })
 
+//server listening on port 8080
 server.listen(port, () => {
     console.log('listening on port ' + port)
 });
 
+//matches vote in the object array, if match incremented count by 1
 function incrementPoll(vote) {
     for ( let i = 0; i < pollObj.options.length; i++ )
     {
@@ -47,20 +52,22 @@ function incrementPoll(vote) {
     }
 };
 
+//returns all text options in an array
 function getLabels() {
     let labels = [];
     for ( let i = 0; i < pollObj.options.length; i ++ ) {
         labels.push(pollObj.options[i].text);
     }
-    console.log(labels)
+    //console.log(labels)
     return labels
 }
 
+//returns all value options in an array
 function getValues() {
     let values = [];
     for ( let i = 0; i < pollObj.options.length; i ++ ) {
         values.push(pollObj.options[i].count);
     }
-    console.log(values)
+    //console.log(values)
     return values
 }
